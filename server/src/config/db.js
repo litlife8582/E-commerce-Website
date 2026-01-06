57a71d4d179bf24f8a7e04e735f5b07da95c2503
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-01-06 19:37:39 +0530

    Initializing the e-commerce website project

---
+++ server/src/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+
+//mongoose.connect returns a promise hence we use an async function
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);//Log success incase DB is connected
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);//Log error incase BD fails to connect
+    process.exit(1);
+  }
+};
+
+export default connectDB;
