f0ae930f80df434ff96c0a535b01eadb8d6b09d3
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-01-11 21:53:04 +0530

    User schema modelling done

---
+++ server/src/models/User.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema(
+    {
+        Name:{
+            type:String,
+            required:true
+        },
+        Password:{
+            type:String,
+            required:true
+        },
+        Address:{
+            House:{
+                type:String,
+                required:true
+            },
+            Neighbourhood:{
+                type:String,
+                required:true
+            },
+            City:{
+                type:String,
+                required:true
+            },
+            District:{
+                type:String,
+                required:true
+            },
+            Pincode:{
+                type:Number,
+                required:true
+            },
+            State:{
+                type:String,
+                required:true
+            },
+            Country:{
+                type:String,
+                required:true
+            },
+            Role:{
+                type:String,
+                enum:["buyer","seller"],
+                required:true
+            }
+        }
+    },{timestamps: true}
+);
+
+export const User=mongoose('User',userSchema);
