ca0023d2c984970bd5033c9f32b492a38d49ab19
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-01-13 22:17:41 +0530

    Order Schema built

---
+++ server/src/models/Order.js
@@ -0,0 +1,47 @@
+// user ref, orderItems (product ref + qty + price), shippingAddress, paymentInfo, totalPrice, status, timestamps
+
+import mongoose from "mongoose";
+
+const orderSchema=new mongooseSchema(
+    {
+        userRef:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        },
+        orderItems:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Product'
+        },
+        Address:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        },
+        paymentInfo:{
+            method:{
+                type:String,
+                enum:['UPI','COD']
+            },
+            upiID:{
+                type:String,
+                required:function(){
+                    return['UPI'].includes(this.method)
+                }
+            },
+            status:{
+                type:String,
+                default:"Pending"
+            },
+            paidAt:{
+                type:Date,
+                required:true
+            }
+        },
+        deliveryStatus:{
+            type: String,
+            enum:["Shipped","Out for Delivery","Delivered","Cancelled"],
+            required:true
+        }
+    },{timestamps:true}
+)
+
+export const Order=mongooseSchema('Order',orderSchema)
