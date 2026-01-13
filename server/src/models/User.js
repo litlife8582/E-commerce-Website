935e1bd1f5afdb0ff9d7dca6f76ae5ae85a08443
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-01-13 22:33:26 +0530

    User Schema changed for auth

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -6,6 +6,13 @@ const userSchema=new mongoose.Schema(
             type:String,
             required:true
         },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true
+
+        },
         Password:{
             type:String,
             required:true
